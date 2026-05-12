4e306b9e0bf43875594e5522ff8ebc4828d99c98
mhriyad99 <mhriyad98@gmail.com>
2026-05-12 10:48:05 +0600

    May 12: initialize db

--- app/db/database.py
+++ app/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sess
 from sqlalchemy.orm import DeclarativeBase
 from app.core.config import settings
 
-DATABASE_URL = (f"postgres+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:"
+DATABASE_URL = (f"postgresql+asyncpg://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:"
                 f"{settings.DB_PORT}/{settings.DB_NAME}")
 
 engine = create_async_engine(
--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
-app = FastAPI()
+from app.db.database import init_db, close_db
+from app.db import models
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await init_db()
+    yield
+    await close_db()
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get("/")
 async def root():
